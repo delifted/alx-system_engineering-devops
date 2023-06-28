52ea2c66c77df9b444ce16ad3834fef94f3423cc
Oluwasegun Oyelola <oluwasegun.oyelola01@gmail.com>
2023-06-28 21:09:39 +0100

    $commit_message

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,28 @@
+# Puppet manifest for nginx installation and some configurations
+
+package {'nginx':
+  ensure => 'present',
+}
+
+exec {'install':
+  command  => 'sudo apt-get update -y ; sudo apt-get -y install nginx',
+  provider => shell,
+
+}
+
+exec {'Home page':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+
+}
+
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.github.com/delifted\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+
+}
+
+exec {'restart nginx':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+
+}
